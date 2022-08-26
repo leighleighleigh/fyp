fa168b167870fb69f3ac83974f7eac915083414e
Leigh <leighcharlesoliver@gmail.com>
2022-08-26 16:00:38 +1000

    Added first PlatformIO project. Updated README to test GH-style markdown

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# fyp
+# Sensors In Green Infrastructure
+> *Application of low-cost electronic sensors to the challenge of biofilter maintenance* <br>
+> An Engineering Final Year Project of Monash University <br>
+> Authored by Leigh Oliver, supervised by Dr. Brandon Winfrey <br>
---
+++ pio_column_rawdata_json/src/main.ino
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "chameleon.h"
+#include "smt.h"
+
+// SMT100 analog inputs
+#define SMT_TMP A0
+#define SMT_SOIL A1
+
+// Chameleon sensor uses two pins
+#define CHMLN_0_A A2
+#define CHMLN_0_B A3
+#define CHMLN_1_A A4
+#define CHMLN_1_B A5
+
+long sampleCount = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(SMT_TMP,INPUT);
+  pinMode(SMT_SOIL,INPUT);
+  // Wait for connection
+  while(!Serial) continue;
+}
+
+void loop()
+{
+  // The json document variable 
+  StaticJsonDocument<256> doc;
+  JsonObject root = doc.to<JsonObject>();
+
+  // Add timestamp information to the json document
+  doc["sample_n"] = sampleCount;
+  doc["sample_ms"] = millis();
+
+  // Read chameleon 0
+  JsonObject chmn0 = root.createNestedObject("chameleon_lower");
+  readChameleonToJSON(CHMLN_0_A,CHMLN_0_B,chmn0);
+
+  // Read chameleon 1
+  JsonObject chmn1 = root.createNestedObject("chameleon_upper");
+  readChameleonToJSON(CHMLN_1_A,CHMLN_1_B,chmn1);
+
+  // Read SMT100 analog lines
+  JsonObject smt0 = root.createNestedObject("smt_upper");
+  readSMTToJSON(SMT_TMP,SMT_SOIL,smt0);
+
+  // Write the JSON output to serial
+  serializeJson(doc, Serial);
+  Serial.println();
+
+  // Increment sample counter
+  sampleCount++;
+  // Delay so the sensor can settle 
+  delay(1000);
+}
