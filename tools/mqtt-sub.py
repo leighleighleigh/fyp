18f08529d78fdd85a60935f45176ddfa520c3258
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-08-28 14:37:54 +1000

    MQTT to CSV script draft

--- tools/mqtt-sub.py
+++ tools/mqtt-sub.py
@@ -4,21 +4,39 @@
 import json
 from time import sleep
 from Adafruit_IO import Client
+import matplotlib.pyplot as plt
+
+# Connect to Adafruit IO
 aio = Client(username="leighleighleigh",key='aio_wrSI324kBhMYCBMYioznPAinojqs')
 
-# Get list of feeds.
-#feeds = aio.feeds()
-# Print out the feed names:
-#for f in feeds:
-    #print('Feed: {0}'.format(f.name))
-
-data = aio.data("bosl-mqtt")
+# Build a dictionary of lists, each key is a 'nickname'+IMEI of a board
+data = list(aio.data("bosl-mqtt"))
+data.reverse()
+fleet = {}
 
 for d in data:
     try:
         dj = json.loads(d.value)
         if "IMEI" in dj.keys():
-            #print(dj)
+            nn = dj["nickname"]
+            if nn not in fleet.keys():
+                fleet[nn] = []
+
+            fleet[nn].append(dj["battery_mv"])
             print(json.dumps(dj, indent=2, sort_keys=True))
     except:
         pass
+
+# Write to file
+for n in fleet.keys():
+    fname = f"{nn}_battery.csv"
+    with open(fname,"w") as f:
+        #f.write("sample_n, battery_mv\n")
+        datan = list(enumerate(fleet[n]))
+        for idx, i in datan:
+            f.write(f"{idx},{i}\n")
+
+        x,y = [x[0] for x in datan],[x[1] for x in datan]
+        plt.plot(x,y)
+
+    plt.show()
