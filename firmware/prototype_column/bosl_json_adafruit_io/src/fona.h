82c3a708039d732481d4eb8648c7208b8263eb21
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-08-28 13:40:11 +1000

    Working MQTT publish to Adafruit IO

--- firmware/prototype_column/bosl_json_adafruit_io/src/fona.h
+++ firmware/prototype_column/bosl_json_adafruit_io/src/fona.h
@@ -1,21 +1,17 @@
 #include <Arduino.h>
 #include "Adafruit_FONA.h" // https://github.com/botletics/SIM7000-LTE-Shield/tree/master/Code
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_FONA.h"
+// #include "Adafruit_MQTT.h"
+// #include "Adafruit_MQTT_FONA.h"
 #include "ArduinoJson.h"
 #include <SoftwareSerial.h>
 
 #define SIMCOM_7000
 #define FONA_PWRKEY 4
-// #define FONA_RST 7 // unsure if used on BoSL
 #define FONA_DTR 5 // Connect with solder jumper
-//#define FONA_RI 9 // Need to enable via AT commands
-#define FONA_TX 9 // Microcontroller RX
-#define FONA_RX 8 // Microcontroller TX
+#define FONA_TX 9  // Microcontroller RX
+#define FONA_RX 8  // Microcontroller TX
 
-SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
-SoftwareSerial *fonaSerial = &fonaSS;
-
+SoftwareSerial fonaSerial = SoftwareSerial(FONA_TX, FONA_RX);
 Adafruit_FONA_LTE fona = Adafruit_FONA_LTE();
 
 /************************* MQTT SETUP *********************************/
@@ -24,77 +20,34 @@ Adafruit_FONA_LTE fona = Adafruit_FONA_LTE();
 #define AIO_SERVER "io.adafruit.com"
 #define AIO_SERVERPORT 1883
 #define AIO_USERNAME "leighleighleigh"
-#define AIO_KEY "ef686e14f1ad4ae1bf7b9cadc5a8213d"
-
-// Setup the FONA MQTT class by passing in the FONA class and MQTT server and login details.
-Adafruit_MQTT_FONA mqtt(&fona, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-
-// How many transmission failures in a row we're OK with before reset
-uint8_t txfailures = 5;
-
-/****************************** MQTT FEEDS ***************************************/
-Adafruit_MQTT_Publish feed_location = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/bosl_mqtt/json");
+#define AIO_KEY "aio_wrSI324kBhMYCBMYioznPAinojqs"
 
 /****************************** OTHER STUFF ***************************************/
-uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
-char imei[16] = {2}; // Use this for device ID
+char imei[16] = {0}; // Use this for device ID
 uint8_t type;
 float latitude, longitude, speed_kph, heading, altitude, second;
 uint16_t year;
 uint8_t month, day, hour, minute;
-uint8_t counter = 0;
+
 // char PIN[5] = "1234"; // SIM card PIN
 
 // NOTE: Keep the buffer sizes as small as possible, espeially on
 // Arduino Uno which doesn't have much computing power to handle
 // large buffers. On Arduino Mega you shouldn't have to worry much.
-char locBuff[250];
+char locBuff[200];
 
-void moduleSetup();
-void MQTT_connect();
-void MQTT_publish_checkSuccess(Adafruit_MQTT_Publish &feed, const char *feedContent);
 bool netStatus();
+void moduleSetup();
 
 void setupFONA()
 {
-    // pinMode(FONA_RST, OUTPUT);
-    // digitalWrite(FONA_RST, HIGH); // Default state
+    fona.powerOn(FONA_PWRKEY);
+    moduleSetup();
 
-    fona.powerOn(FONA_PWRKEY); // Power on the module
-    moduleSetup();             // Establishes first-time serial comm and prints IMEI
-
-    // Unlock SIM card if needed
-    // Remember to uncomment the "PIN" variable definition above
-    /*
-    if (!fona.unlockSIM(PIN)) {
-      Serial.println(F("Failed to unlock SIM card"));
-    }
-    */
-
-    // Set modem to full functionality
-    fona.setFunctionality(0); // AT+CFUN=1
-    delay(1000);
-    fona.setFunctionality(1); // AT+CFUN=1
-    fona.setFunctionality(6); // AT+CFUN=1
-    delay(10000);
-    fona.setFunctionality(1); // AT+CFUN=1
+    fona.setFunctionality(1);
 
     // fona.setNetworkSettings(F("mdata.net.au"));
-    fona.setNetworkSettings(F("telstra.internet"));
-
-    /*
-    // Other examples of some things you can set:
-    fona.setPreferredMode(38); // Use LTE only, not 2G
-    fona.setPreferredLTEMode(1); // Use LTE CAT-M only, not NB-IoT
-    fona.setOperatingBand("CAT-M", 12); // AT&T uses band 12
-  //  fona.setOperatingBand("CAT-M", 13); // Verizon uses band 13
-    fona.enableRTC(true);
-
-    fona.enableSleepMode(true);
-    fona.set_eDRX(1, 4, "0010");
-    fona.enablePSM(true);
-
-    */
+    fona.setNetworkSettings(F("mdata.net.au"));
 
     // Perform first-time GPS/data setup if the shield is going to remain on,
     // otherwise these won't be enabled in loop() and it won't work!
@@ -106,45 +59,19 @@ void setupFONA()
     }
     Serial.println(F("Turned on GPS!"));
 
-    // Disable data just to make sure it was actually off so that we can turn it on
-    if (!fona.enableGPRS(false))
-        Serial.println(F("Failed to disable data!"));
+    // turn GPRS off
+    // if (!fona.enableGPRS(false))
+    // {
+    //     Serial.println(F("Failed to turn off GPRS..."));
+    //     delay(2000);
+    // }
 
-    // Turn on data
-    while (!fona.enableGPRS(true))
-    {
-        Serial.println(F("Failed to enable data, retrying..."));
-        delay(2000); // Retry every 2s
-    }
-    Serial.println(F("Enabled data!"));
-}
-
-void readGPStoJSON(JsonObject& gps_obj)
-{
-    // Get a fix on location, try every 2s
-    // Use the top line if you want to parse UTC time data as well, the line below it if you don't care
-    //  while (!fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude, &year, &month, &day, &hour, &minute, &second)) {
-    while (!fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude))
-    {
-        Serial.println(F("Failed to get GPS location, retrying..."));
-        delay(2000); // Retry every 2s
-    }
-
-    // Store the results nicely into a JSON document
-    gps_obj["lat"] = latitude;
-    gps_obj["lon"] = longitude;
-    gps_obj["ele"] = altitude;
-}
-
-void loopFONA()
-{
-    // The json document variable 
-    StaticJsonDocument<256> doc;
-    JsonObject root = doc.to<JsonObject>();
-
-    // Add timestamp information to the json document
-    doc["value"] = millis();
-
+    // turn GPRS on
+    // if (!fona.enableGPRS(true))
+    // {
+    //     Serial.println(F("Failed to turn on GPRS..."));
+    //     delay(2000);
+    // }
 
     // Connect to cell network and verify connection
     // If unsuccessful, keep retrying every 2s until a connection is made
@@ -154,27 +81,94 @@ void loopFONA()
         delay(2000); // Retry every 2s
     }
     Serial.println(F("Connected to cell network!"));
-    
+}
+
+void readGPStoJSON(JsonObject &gps_obj)
+{
+    // Get a fix on location, try every 2s
+    // Use the top line if you want to parse UTC time data as well, the line below it if you don't care
+    //  while (!fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude, &year, &month, &day, &hour, &minute, &second)) {
+    if (!fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude))
+    {
+        Serial.println(F("Failed to get GPS location..."));
+    }
+
+    // Store the results nicely into a JSON document
+    gps_obj["lat"] = latitude;
+    gps_obj["lon"] = longitude;
+    gps_obj["ele"] = altitude;
+    // gps_obj["lat"] = -37.78;
+    // gps_obj["lon"] = 144.944;
+    // gps_obj["ele"] = 10.0;
+}
+
+void loopFONA()
+{
+    // The json document variable
+    StaticJsonDocument<128> doc;
+    JsonObject root = doc.to<JsonObject>();
+
+    // Add timestamp information to the json document
+    doc["value"] = millis();
+
     // Read GPS
-    JsonObject fonaGPS0 = root.createNestedObject("gps");
-    readGPStoJSON(fonaGPS0);
+    // JsonObject fonaGPS0 = root.createNestedObject("gps");
+    readGPStoJSON(root);
 
     // Write the JSON output to serial, and the buffer
-    serializeJson(doc, locBuff);
-    serializeJson(doc, Serial);
-    Serial.println();
+    // serializeJson(doc, locBuff);
+    serializeJson(doc, locBuff, 200);
 
-    // Ensure the connection to the MQTT server is alive (this will make the first
-    // connection and automatically reconnect when disconnected). See the MQTT_connect
-    // function definition further below.
-    MQTT_connect();
+    // Also construct a combined, comma-separated location array
+    // sprintf(locBuff, "%s", String("99").c_str());
 
-    // Now publish all the data to different feeds!
-    // The MQTT_publish_checkSuccess handles repetitive stuff.
-    // You can see the function near the end of this sketch.
-    // For the Adafruit IO dashboard map we send the combined lat/long buffer
-    MQTT_publish_checkSuccess(feed_location, locBuff);
+    // Disable data just to make sure it was actually off so that we can turn it on
+    // fona.openWirelessConnection(false);
 
+    // Open wireless connection if not already activated
+    if (!fona.wirelessConnStatus())
+    {
+        while (!fona.openWirelessConnection(true))
+        {
+            Serial.println(F("Failed to enable connection, retrying..."));
+            delay(2000); // Retry every 2s
+        }
+        Serial.println(F("Enabled data!"));
+    }
+    else
+    {
+        Serial.println(F("Data already enabled!"));
+    }
+
+    // If not already connected, connect to MQTT
+    if (!fona.MQTT_connectionStatus())
+    {
+        // Set up MQTT parameters (see MQTT app note for explanation of parameter values)
+        fona.MQTT_setParameter("URL", AIO_SERVER, AIO_SERVERPORT);
+        // Set up MQTT username and password if necessary
+        fona.MQTT_setParameter("USERNAME", AIO_USERNAME);
+        fona.MQTT_setParameter("PASSWORD", AIO_KEY);
+        //    fona.MQTTsetParameter("KEEPTIME", 30); // Time to connect to server, 60s by default
+
+        Serial.println(F("Connecting to MQTT broker..."));
+        if (!fona.MQTT_connect(true))
+        {
+            Serial.println(F("Failed to connect to broker!"));
+        }
+    }
+    else
+    {
+        Serial.println(F("Already connected to MQTT server!"));
+    }
+
+    // Now publish all the GPS and temperature data to their respective topics!
+    // Parameters for MQTT_publish: Topic, message (0-512 bytes), message length, QoS (0-2), retain (0-1)
+    if (!fona.MQTT_publish("leighleighleigh/feeds/bosl-mqtt", locBuff, strlen(locBuff), 1, 0))
+        Serial.println(F("Failed to publish!")); // Send GPS location
+
+    // Disconn
+    // fona.MQTT_connect(false);
+    // fona.openWirelessConnection(false);
 }
 
 void moduleSetup()
@@ -184,44 +178,18 @@ void moduleSetup()
     // When the module is on it should communicate right after pressing reset
 
     // Software serial:
-    fonaSS.begin(115200); // Default SIM7000 shield baud rate
+    fonaSerial.begin(115200); // Default SIM7000 shield baud rate
 
     Serial.println(F("Configuring to 9600 baud"));
-    fonaSS.println("AT+IPR=9600"); // Set baud rate
-    delay(100);                    // Short pause to let the command run
-    fonaSS.begin(9600);
-    if (!fona.begin(fonaSS))
+    fonaSerial.println("AT+IPR=9600"); // Set baud rate
+    delay(100);                        // Short pause to let the command run
+    fonaSerial.begin(9600);
+    if (!fona.begin(fonaSerial))
     {
         Serial.println(F("Couldn't find FONA"));
         while (1)
             ; // Don't proceed if it couldn't find the device
     }
-
-    // Hardware serial:
-    /*
-    fonaSerial->begin(115200); // Default SIM7000 baud rate
-
-    if (! fona.begin(*fonaSerial)) {
-      DEBUG_PRINTLN(F("Couldn't find SIM7000"));
-    }
-    */
-
-    // The commented block of code below is an alternative that will find the module at 115200
-    // Then switch it to 9600 without having to wait for the module to turn on and manually
-    // press the reset button in order to establish communication. However, once the baud is set
-    // this method will be much slower.
-    /*
-    fonaSerial->begin(115200); // Default LTE shield baud rate
-    fona.begin(*fonaSerial); // Don't use if statement because an OK reply could be sent incorrectly at 115200 baud
-
-    Serial.println(F("Configuring to 9600 baud"));
-    fona.setBaudrate(9600); // Set to 9600 baud
-    fonaSerial->begin(9600);
-    if (!fona.begin(*fonaSerial)) {
-      Serial.println(F("Couldn't find modem"));
-      while(1); // Don't proceed if it couldn't find the device
-    }
-    */
 
     type = fona.type();
     Serial.println(F("FONA is OK"));
@@ -272,7 +240,6 @@ void moduleSetup()
     }
 }
 
-
 bool netStatus()
 {
     int n = fona.getNetworkStatus();
@@ -297,43 +264,4 @@ bool netStatus()
         return false;
     else
         return true;
-}
-
-// Function to connect and reconnect as necessary to the MQTT server.
-// Should be called in the loop function and it will take care if connecting.
-void MQTT_connect()
-{
-    int8_t ret;
-
-    // Stop if already connected.
-    if (mqtt.connected())
-    {
-        return;
-    }
-
-    Serial.println("Connecting to MQTT... ");
-
-    while ((ret = mqtt.connect()) != 0)
-    { // connect will return 0 for connected
-        Serial.println(mqtt.connectErrorString(ret));
-        Serial.println("Retrying MQTT connection in 5 seconds...");
-        mqtt.disconnect();
-        delay(5000); // wait 5 seconds
-    }
-    Serial.println("MQTT Connected!");
-}
-
-void MQTT_publish_checkSuccess(Adafruit_MQTT_Publish &feed, const char *feedContent)
-{
-    Serial.println(F("Sending data..."));
-    if (!feed.publish(feedContent))
-    {
-        Serial.println(F("Failed"));
-        txfailures++;
-    }
-    else
-    {
-        Serial.println(F("OK!"));
-        txfailures = 0;
-    }
 }
--- firmware/prototype_column/bosl_json_adafruit_io/src/main.ino
+++ firmware/prototype_column/bosl_json_adafruit_io/src/main.ino
@@ -14,8 +14,6 @@
 #define CHMLN_1_A A4
 #define CHMLN_1_B A5
 
-long sampleCount = 0;
-
 void setup()
 {
   Serial.begin(9600);
@@ -29,39 +27,9 @@ void setup()
   setupFONA();
 }
 
-void readSensorsToJSON()
-{
-  // The json document variable 
-  StaticJsonDocument<256> doc;
-  JsonObject root = doc.to<JsonObject>();
-
-  // Add timestamp information to the json document
-  doc["sample_n"] = sampleCount;
-  doc["sample_ms"] = millis();
-
-  // Read chameleon 0
-  JsonObject chmn0 = root.createNestedObject("chameleon_lower");
-  readChameleonToJSON(CHMLN_0_A,CHMLN_0_B,chmn0);
-
-  // Read chameleon 1
-  JsonObject chmn1 = root.createNestedObject("chameleon_upper");
-  readChameleonToJSON(CHMLN_1_A,CHMLN_1_B,chmn1);
-
-  // Read SMT100 analog lines
-  JsonObject smt0 = root.createNestedObject("smt_upper");
-  readSMTToJSON(SMT_TMP,SMT_SOIL,smt0);
-
-  // Write the JSON output to serial
-  serializeJson(doc, Serial);
-  Serial.println();
-
-  // Increment sample counter
-  sampleCount++;
-}
-
 void loop()
 {
   loopFONA();
   // Delay so the sensor can settle 
-  delay(1000);
+  delay(10000);
 }
