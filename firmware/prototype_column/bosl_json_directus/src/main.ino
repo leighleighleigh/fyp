fa3dca888a405d19148ef9e4d6b009b7f8eda6c0
Leigh <leighcharlesoliver@gmail.com>
2022-09-02 09:13:57 +1000

    Error 601 on directus

---
+++ firmware/prototype_column/bosl_json_directus/src/main.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "fona.h"
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // Setup FONA
+  setupFONA();
+}
+
+void loop()
+{
+  loopFONA();
+  // Delay so the sensor can settle 
+  delay(5000);
+}
