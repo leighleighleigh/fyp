6265575d7eb23c9a4b3a473b113019a18af69777
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-09-03 23:04:45 +1000

    Not working yet, trying CSV

--- firmware/prototype_column/bosl_json_directus/src/schema.h
+++ firmware/prototype_column/bosl_json_directus/src/schema.h
@@ -1,3 +1,4 @@
 #include <Arduino.h>
 
-const char * column_data_schema = "{\"bosl_name\":\"%s\",\"bosl_imei\":\"%s\",\"bosl_battery_mv\":%d,\"bosl_bootup_timestamp\":%s,\"chmln_top_raw_a\":%d,\"chmln_top_raw_b\":%d,\"chmln_top_raw_avg\":%f,\"chmln_top_resistance\":%d,\"chmln_bot_raw_a\":%d,\"chmln_bot_raw_b\":%d,\"chmln_bot_raw_avg\":%f,\"chmln_bot_resistance\":%d,\"ds18b20_bot_temperature\":%f,\"ds18b20_top_temperature\":%f,\"smt100_top_vwc\":%f,\"smt100_top_temperature\":%f,\"smt100_bot_vwc\":%d,\"smt100_bot_temperature\":%d}";
+const char *column_data_schema = "{\"bosl_name\":\"%s\",\"bosl_imei\":\"%s\",\"bosl_battery_mv\":%d,\"bosl_bootup_timestamp\":%s,\"chmln_top_raw_a\":%d,\"chmln_top_raw_b\":%d,\"chmln_top_raw_avg\":%d,\"chmln_top_resistance\":%d,\"chmln_bot_raw_a\":%d,\"chmln_bot_raw_b\":%d,\"chmln_bot_raw_avg\":%s,\"chmln_bot_resistance\":%d,\"ds18b20_bot_temperature\":%s,\"ds18b20_top_temperature\":%s,\"smt100_top_vwc\":%s,\"smt100_top_temperature\":%s,\"smt100_bot_vwc\":%d,\"smt100_bot_temperature\":%d}";
+// const char *column_data_schema = "{\"bosl_name\":\"%s\",\"bosl_imei\":\"%s\",\"bosl_battery_mv\":%d}";
