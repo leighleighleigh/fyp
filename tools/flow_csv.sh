2aeea215d4ee591443b7bf1b0d279f7e1b5d65c7
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-09-08 00:07:37 +1000

    Fake bosl data maker

--- tools/flow_csv.sh
+++ tools/flow_csv.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 URL=https://cms.leigh.sh/flows/trigger/625c3333-48bf-4397-a5a6-a0d72e204b6f
-DATA='{"data":"1,2,3,4,5,hi"}'
+DATA="$(jq -nc '.data="1,2,3,4,5,hi"')"
+TOKEN='nNgr-OJA-K2cNLkfZWQ0B-Xzlrkb9coN'
+MIME="Content-Type: application/json"
+#MIME='Content-Type: text/plain'
 
-curl -X POST -H 'Authorization: Bearer zjl1TenoVD4ld8UHkWCrdzKb6CfnM32Q' -H 'Content-Type: application/json' -d $DATA ${URL}
+curl -X POST -H "Authorization: Bearer ${TOKEN}" -H "${MIME}" -d $DATA ${URL}
