519559a20863b22f7e3530cff34a7815f96fe96c
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-09-09 15:01:47 +1000

    Working HTTP calls using Boteltic SIM7000 library!

---
+++ firmware/prototype_column/bosl_json_directus_raw_at/src/main.ino
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "chameleon.h"
+#include "smt.h"
+#include "fona.h"
+#include "sim.h"
+
+// SMT100 analog inputs
+#define SMT_TMP A0
+#define SMT_SOIL A1
+
+// Chameleon sensor uses two pins
+#define CHMLN_0_A A2
+#define CHMLN_0_B A3
+#define CHMLN_1_A A4
+#define CHMLN_1_B A5
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(SMT_TMP,INPUT);
+  pinMode(SMT_SOIL,INPUT);
+  // Wait for connection
+  // while(!Serial) continue;
+
+  // Setup FONA
+  // setupSIM();
+  setupFONA();
+}
+
+void loop()
+{
+  // loopSIM();
+  loopFONA();
+  // Delay so the sensor can settle 
+  delay(1000);
+}
