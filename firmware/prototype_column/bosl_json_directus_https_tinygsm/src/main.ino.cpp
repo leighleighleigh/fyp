31933c4e023ac03b289e56c78a4466724035eb3a
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-09-21 15:20:11 +1000

    Added streamdebugger and created a simplified TinyGSM HTTPS example (unworking)

--- firmware/prototype_column/bosl_json_directus_https_tinygsm/src/main.ino.cpp
+++ firmware/prototype_column/bosl_json_directus_https_tinygsm/src/main.ino.cpp
@@ -1,112 +1,86 @@
-/**************************************************************
- *
- * This sketch connects to a website and downloads a page.
- * It can be used to perform HTTP/RESTful API calls.
- *
- * For this example, you need to install ArduinoHttpClient library:
- *   https://github.com/arduino-libraries/ArduinoHttpClient
- *   or from http://librarymanager/all#ArduinoHttpClient
- *
- * TinyGSM Getting Started guide:
- *   https://tiny.cc/tinygsm-readme
- *
- * SSL/TLS is not yet supported on the Quectel modems
- * The A6/A7/A20 and M590 are not capable of SSL/TLS
- *
- * For more HTTP API examples, see ArduinoHttpClient library
- *
- * NOTE: This example may NOT work with the XBee because the
- * HttpClient library does not empty to serial buffer fast enough
- * and the buffer overflow causes the HttpClient library to stall.
- * Boards with faster processors may work, 8MHz boards will not.
- **************************************************************/
-
+# 1 "/tmp/tmpq_abrw24"
+#include <Arduino.h>
+# 1 "/home/leigh/Documents/GitHub/fyp/firmware/prototype_column/bosl_json_directus_https_tinygsm/src/main.ino"
+# 24 "/home/leigh/Documents/GitHub/fyp/firmware/prototype_column/bosl_json_directus_https_tinygsm/src/main.ino"
 #define PWRKEY 4
-#define DTR 5 // Connect with solder jumper
-#define BOSL_RX 9 // Microcontroller RX
-#define BOSL_TX 8 // Microcontroller TX
-#define __AVR_ATmega328P__
+#define DTR 5
+#define BOSL_RX 9
+#define BOSL_TX 8
+#define __AVR_ATmega328P__ 
 
-// Select your modem:
-// #define TINY_GSM_MODEM_SIM800
-// #define TINY_GSM_MODEM_SIM808
-// #define TINY_GSM_MODEM_SIM868
-#define TINY_GSM_MODEM_SIM7000SSL
-// #define TINY_GSM_MODEM_SIM7080
-// #define TINY_GSM_MODEM_UBLOX
-// #define TINY_GSM_MODEM_SARAR4
-// #define TINY_GSM_MODEM_ESP8266
-// #define TINY_GSM_MODEM_XBEE
-// #define TINY_GSM_MODEM_SEQUANS_MONARCH
 
-// Set serial for debug console (to the Serial Monitor, default speed 115200)
+
+
+
+#define TINY_GSM_MODEM_SIM7000SSL 
+# 43 "/home/leigh/Documents/GitHub/fyp/firmware/prototype_column/bosl_json_directus_https_tinygsm/src/main.ino"
 #define SerialMon Serial
 
-// Set serial for AT commands (to the module)
-// Use Hardware Serial on Mega, Leonardo, Micro
+
+
 #ifndef __AVR_ATmega328P__
 #define SerialAT Serial1
 
-// or Software Serial on Uno, Nano
+
 #else
 #include <SoftwareSerial.h>
-SoftwareSerial SerialAT(BOSL_RX, BOSL_TX);  // RX, TX
+SoftwareSerial SerialAT(BOSL_RX, BOSL_TX);
 #endif
 
-// Increase RX buffer to capture the entire response
-// Chips without internal buffering (A6/A7, ESP8266, M590)
-// need enough space in the buffer for the entire response
-// else data will be lost (and the http library will fail).
+
+
+
+
 #if !defined(TINY_GSM_RX_BUFFER)
 #define TINY_GSM_RX_BUFFER 200
 #endif
 
-// See all AT commands, if wanted
-#define DUMP_AT_COMMANDS
 
-// Define the serial console for debug prints, if needed
+#define DUMP_AT_COMMANDS 
+
+
 #define TINY_GSM_DEBUG SerialMon
-// #define LOGGING  // <- Logging is for the HTTP library
 
-// Range to attempt to autobaud
-// NOTE:  DO NOT AUTOBAUD in production code.  Once you've established
-// communication, set a fixed baud rate using modem.setBaud(#).
+
+
+
+
 #define GSM_AUTOBAUD_MIN 9600
 #define GSM_AUTOBAUD_MAX 115200
 
-// Add a reception delay, if needed.
-// This may be needed for a fast processor at a slow baud rate.
+
+
 #define TINY_GSM_YIELD() { delay(2); }
 
-// Define how you're planning to connect to the internet.
-// This is only needed for this example, not in other code.
+
+
 #define TINY_GSM_USE_GPRS true
 #define TINY_GSM_USE_WIFI false
 
-// set GSM PIN, if any
+
 #define GSM_PIN ""
 
-// flag to force SSL client authentication, if needed
-// #define TINY_GSM_SSL_CLIENT_AUTHENTICATION
 
-// Your GPRS credentials, if any
-const char apn[]      = "mdata.net.au";
+
+
+
+const char apn[] = "mdata.net.au";
 const char gprsUser[] = "";
 const char gprsPass[] = "";
 
-// Your WiFi connection credentials, if applicable
+
 const char wifiSSID[] = "YourSSID";
 const char wifiPass[] = "YourWiFiPass";
 
-// Server details
-const char server[]   = "vsh.pp.ua";
+
+const char server[] = "vsh.pp.ua";
 const char resource[] = "/TinyGSM/logo.txt";
-const int  port       = 443;
+const int port = 443;
 
 #include <TinyGsmClient.h>
 #include <ArduinoHttpClient.h>
 
-// Just in case someone defined the wrong thing..
+
 #if TINY_GSM_USE_GPRS && not defined TINY_GSM_MODEM_HAS_GPRS
 #undef TINY_GSM_USE_GPRS
 #undef TINY_GSM_USE_WIFI
@@ -123,78 +97,82 @@ const int  port       = 443;
 #ifdef DUMP_AT_COMMANDS
 #include <StreamDebugger.h>
 StreamDebugger debugger(SerialAT, SerialMon);
-TinyGsm        modem(debugger);
+TinyGsm modem(debugger);
 #else
-TinyGsm        modem(SerialAT);
+TinyGsm modem(SerialAT);
 #endif
 
 TinyGsmClientSecure client(modem);
-HttpClient          http(client, server, port);
-
+HttpClient http(client, server, port);
+void simOn();
+void simOff();
+void setup();
+void loop();
+#line 134 "/home/leigh/Documents/GitHub/fyp/firmware/prototype_column/bosl_json_directus_https_tinygsm/src/main.ino"
 void simOn() {
-  //powers on SIM7000
-  
-  //do check for if sim is on
+
+
+
   pinMode(PWRKEY, OUTPUT);
   pinMode(BOSL_TX, OUTPUT);
   digitalWrite(BOSL_TX, HIGH);
   pinMode(BOSL_RX, INPUT_PULLUP);
   digitalWrite(PWRKEY, LOW);
-  delay(1000); // For SIM7000
+  delay(1000);
   digitalWrite(PWRKEY, HIGH);
   delay(4000);
 }
 
 void simOff() {
-  //powers off SIM7000
-  
-  //  TX / RX pins off to save power
+
+
+
   digitalWrite(BOSL_TX, LOW);
   digitalWrite(BOSL_RX, LOW);
   digitalWrite(PWRKEY, LOW);
-  delay(1200); // For SIM7000
+  delay(1200);
   digitalWrite(PWRKEY, HIGH);
   delay(2000);
 }
 
 
 void setup() {
-  // Set console baud rate
+
   SerialMon.begin(115200);
   delay(10);
 
-  // !!!!!!!!!!!
-  // Set your reset, enable, power pins here
-  // !!!!!!!!!!!
+
+
+
   simOff();
   simOn();
 
   SerialMon.println("Wait...");
 
-  // Set GSM module baud rate
-  // TinyGsmAutoBaud(SerialAT, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
+
+
   SerialAT.begin(9600);
   delay(6000);
 
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
+
+
   SerialMon.println("Initializing modem...");
   modem.restart();
-  // modem.init();
+
 
   String modemInfo = modem.getModemInfo();
   SerialMon.print("Modem Info: ");
   SerialMon.println(modemInfo);
 
 #if TINY_GSM_USE_GPRS
-  // Unlock your SIM card with a PIN if needed
+
   if (GSM_PIN && modem.getSimStatus() != 3) { modem.simUnlock(GSM_PIN); }
 #endif
 }
 
 void loop() {
 #if TINY_GSM_USE_WIFI
-  // Wifi connection parameters must be set before waiting for the network
+
   SerialMon.print(F("Setting SSID/password..."));
   if (!modem.networkConnect(wifiSSID, wifiPass)) {
     SerialMon.println(" fail");
@@ -205,7 +183,7 @@ void loop() {
 #endif
 
 #if TINY_GSM_USE_GPRS && defined TINY_GSM_MODEM_XBEE
-  // The XBee must run the gprsConnect function BEFORE waiting for network!
+
   modem.gprsConnect(apn, gprsUser, gprsPass);
 #endif
 
@@ -220,7 +198,7 @@ void loop() {
   if (modem.isNetworkConnected()) { SerialMon.println("Network connected"); }
 
 #if TINY_GSM_USE_GPRS
-  // GPRS connection parameters are usually set after network registration
+
   SerialMon.print(F("Connecting to "));
   SerialMon.print(apn);
   if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
@@ -237,7 +215,7 @@ void loop() {
   modem.NTPServerSync();
 
   SerialMon.print(F("Performing HTTPS GET request... "));
-  http.connectionKeepAlive();  // Currently, this is needed for HTTPS
+  http.connectionKeepAlive();
   http.sendHeader(F("Accept: text/plain"));
   http.sendHeader(F("Cache-Control: no-cache"));
   http.sendHeader(F("Transfer-Encoding: chunked"));
@@ -259,7 +237,7 @@ void loop() {
 
   SerialMon.println(F("Response Headers:"));
   while (http.headerAvailable()) {
-    String headerName  = http.readHeaderName();
+    String headerName = http.readHeaderName();
     String headerValue = http.readHeaderValue();
     SerialMon.println("    " + headerName + " : " + headerValue);
   }
@@ -280,7 +258,7 @@ void loop() {
   SerialMon.print(F("Body length is: "));
   SerialMon.println(body.length());
 
-  // Shutdown
+
 
   http.stop();
   SerialMon.println(F("Server disconnected"));
@@ -294,6 +272,6 @@ void loop() {
   SerialMon.println(F("GPRS disconnected"));
 #endif
 
-  // Do nothing forevermore
+
   while (true) { delay(1000); }
 }
