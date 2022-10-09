5e5b9a45d3ca9a3468105c04a574e96b5dd81b34
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-10-09 22:08:30 +1100

    SMT100 read tweaks, 5 minute loop, working DS18B20!

--- firmware/deployed_firmware/bosl_json_directus_http/src/ds18b20.h
+++ firmware/deployed_firmware/bosl_json_directus_http/src/ds18b20.h
@@ -6,24 +6,54 @@
 /********************************************************************/
 // Data wire is plugged into pin 2 on the Arduino 
 #define ONE_WIRE_BUS 2 
+#define ONE_WIRE_BUS_PU 7
 /********************************************************************/
 // Setup a oneWire instance to communicate with any OneWire devices  
 // (not just Maxim/Dallas temperature ICs) 
 OneWire oneWire(ONE_WIRE_BUS); 
 /********************************************************************/
 // Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+DallasTemperature sensors(&oneWire,ONE_WIRE_BUS_PU);
 /********************************************************************/ 
 
-void getAddresses(uint8_t * channelA, uint8_t * channelB)
+// uint8_t tempChannelA, tempChannelB;
+
+// void getAddresses(uint8_t * channelA, uint8_t * channelB)
+// {
+//     uint8_t chA,chB;
+//     sensors.getAddress(&chA,0);
+//     delay(25);
+//     sensors.getAddress(&chB,1);
+//     delay(25);
+//     *channelA = chA;
+//     *channelB = chB;
+// }
+
+void initTemperatures()
 {
-    sensors.getAddress(channelA,0);
-    sensors.getAddress(channelB,1);
+  // Start up the library 
+  sensors.begin();
+  Serial.print("Temperature sensors initialising...");
+
+  uint8_t sensorCount = sensors.getDeviceCount();
+  Serial.print("Sensors found: ");
+  Serial.println(sensorCount);
+
+//   // Get sensor addresses, so that calls to read them are fast!
+//   getAddresses(&tempChannelA,&tempChannelB);
+//   Serial.print("Address A: ");
+//   Serial.println(tempChannelA);
+//   Serial.print("Address B: ");
+//   Serial.println(tempChannelB);
 }
 
-void readTemperatures(uint8_t channelA, uint8_t channelB, float * tempA, float * tempB)
+void readTemperatures(float * tempA, float * tempB)
 {
     sensors.requestTemperatures();
-    *tempA = sensors.getTempC(&channelA); 
-    *tempB = sensors.getTempC(&channelB);
+    sensors.millisToWaitForConversion(100);
+    
+    // *tempA = sensors.getTempC(channelA); 
+    *tempA = sensors.getTempCByIndex(0); 
+    // *tempB = sensors.getTempC(channelB);
+    *tempB = sensors.getTempCByIndex(1); 
 }
