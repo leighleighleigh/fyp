f9893707c8e01097dc3b6a8959c7091789c48680
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-10-13 10:15:54 +1100

    Added log-downloading script

---
+++ tools/boslweb/get.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Retrieves a list of sensor files from the website
+URL=http://bosl.com.au/IoT/wsudwatch/FYP_SGI/
+
+#curl -X GET -H 'Content-Type: application/json' ${URL}
+cd ./downloads/
+wget -r -np -nd -A json,csv -m -k ${URL} 
+ls
+cd ../
