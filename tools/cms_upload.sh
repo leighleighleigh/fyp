c8a5859faebc883de1b01c285d7b6de0407d3a39
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-10-13 16:11:07 +1100

    Plotting utilities!

--- tools/cms_upload.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-URL=https://cms.leigh.sh/items/column_sensors
-DATA=$(cat data.json)
-#echo $DATA
-curl -X POST -H 'Authorization: Bearer zjl1TenoVD4ld8UHkWCrdzKb6CfnM32Q' -H 'Content-Type: application/json' -d $DATA ${URL}
--- tools/url_only_csv_flow.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-#URL=https://cms.leigh.sh/flows/trigger/625c3333-48bf-4397-a5a6-a0d72e204b6f
-URL=https://cms.leigh.sh/flows/trigger/625c3333-48bf-4397-a5a6-a0d72e204b6f?access_token=nNgr-OJA-K2cNLkfZWQ0B-Xzlrkb9coN
-DATA='{"data":"CMS1,2123123,1234"}'
-curl -X POST -H 'Content-Type: application/json' -d $DATA ${URL}
-#curl -X POST -H 'Authorization: Bearer zjl1TenoVD4ld8UHkWCrdzKb6CfnM32Q' -H 'Content-Type: application/json' -d $DATA ${URL}
