19b5c01a9d20246ab792b82d0a3a6e5d01bcaab7
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-10-19 20:55:03 +1100

    Tried raw data array message

--- tools/process_merged_log.py
+++ tools/process_merged_log.py
@@ -147,8 +147,24 @@ with open("merged.mcap","wb") as f_mcap:
         data=schema,
     )
 
+    # with open(Path(__file__).parent / "ColumnDataRawArray.json", "rb") as f:
+    #     schema = f.read()
+
+    # schema_columns = writer.register_schema(
+    #     name="bosl.ColumnDataRawArray",
+    #     encoding=SchemaEncoding.JSONSchema,
+    #     data=schema,
+    # )
+
+    column_channel = writer.register_channel(
+        topic=f"/raw_columns",
+        message_encoding=MessageEncoding.JSON,
+        schema_id=schema_id,
+    )
+
     for idx, gpx in enumerate(deviceprefix):
         device_group = df[df['device_name'].str[0] == gpx]
+
         # Iterate over individual devices
         for cidx, c in enumerate(device_group['device_name'].unique()):
             # g contains the data for a single column within this column group
@@ -168,8 +184,15 @@ with open("merged.mcap","wb") as f_mcap:
             )
 
             # Apply filters to the device data
-            device['chmln_top_cb'] = apply_ohm_to_cb(device, chameleonKey='chmln_top_ohms', tempKey='ds18b20_top_temp_c')
-            device['chmln_bot_cb'] = apply_ohm_to_cb(device, chameleonKey='chmln_bot_ohms', tempKey='ds18b20_bot_temp_c')
+            swapped_channels = ["S2"]
+
+            if c in swapped_channels:
+                device['chmln_top_cb'] = apply_ohm_to_cb(device, chameleonKey='chmln_bot_ohms', tempKey='ds18b20_top_temp_c')
+                device['chmln_bot_cb'] = apply_ohm_to_cb(device, chameleonKey='chmln_top_ohms', tempKey='ds18b20_bot_temp_c')
+            else:
+                device['chmln_top_cb'] = apply_ohm_to_cb(device, chameleonKey='chmln_top_ohms', tempKey='ds18b20_top_temp_c')
+                device['chmln_bot_cb'] = apply_ohm_to_cb(device, chameleonKey='chmln_bot_ohms', tempKey='ds18b20_bot_temp_c')
+
             device['chmln_top_cb_uncalibrated'] = apply_ohm_to_cb(device, chameleonKey='chmln_top_ohms')
             device['chmln_bot_cb_uncalibrated'] = apply_ohm_to_cb(device, chameleonKey='chmln_bot_ohms')
             
@@ -182,29 +205,34 @@ with open("merged.mcap","wb") as f_mcap:
                 device[key] = device[key][device[key]>0].copy()
 
             # Smooth the data into 10 minute rolling averages
-            smoothperiod = '30min'
+            smoothperiod = '10min'
+            # 30 minutes is a little slow
+            #smoothperiod = '30min'
 
-            def smooth(key):
-                device[key] = pd.Series(device[key]).rolling(smoothperiod).mean().copy()
 
             def smoothinline(key):
-                return pd.Series(device[key]).rolling(smoothperiod).mean().copy()
+                return pd.Series(device[key]).rolling(smoothperiod,center=True).mean().copy()
+
+            def smooth(key):
+                device[key] = smoothinline(key)
 
             rmzero('smt_vwc_pct')
+            rmzero('smt_temp_c')
             rmzero('chmln_top_ohms')
-            rmzero('chmln_top_ohms')
+            rmzero('chmln_bot_ohms')
             rmzero('chmln_top_cb')
-            rmzero('chmln_top_cb')
-            rmzero('chmln_bot_cb_uncalibrated')
+            rmzero('chmln_bot_cb')
+            rmzero('chmln_top_cb_uncalibrated')
             rmzero('chmln_bot_cb_uncalibrated')
             rmzero('ds18b20_top_temp_c')
             rmzero('ds18b20_bot_temp_c')
 
             smooth('smt_vwc_pct')
             smooth('smt_temp_c')
+
             smooth('chmln_top_cb')
             smooth('chmln_bot_cb')
-            smooth('chmln_bot_cb_uncalibrated')
+            smooth('chmln_top_cb_uncalibrated')
             smooth('chmln_bot_cb_uncalibrated')
             smooth('ds18b20_top_temp_c')
             smooth('ds18b20_bot_temp_c')
@@ -218,17 +246,21 @@ with open("merged.mcap","wb") as f_mcap:
 
             # Do funky polyfit window thingy
             def rollinglintrend(key):
-                return pd.Series(device[key].copy()).rolling(30).apply(lambda x: polyfit(range(len(x)), x, 1)[0])
+                # each sample is ~2 minutes
+                # 30 samples is 1 hour
+                # 5 samples is 10 minutes
+                return pd.Series(device[key].copy()).rolling(5).apply(lambda x: polyfit(range(len(x)), x, 1)[0])
 
             device['chmln_top_cb_rate'] = rollinglintrend("chmln_top_cb")
             device['chmln_bot_cb_rate'] = rollinglintrend("chmln_bot_cb")
 
-            # # Plot the data
-            # ax = device.plot(y=['chmln_top_cb', 'chmln_bot_cb', 'chmln_top_cb_rolling', 'chmln_bot_cb_rolling',
-            #             'ds18b20_top_temp_c', 'ds18b20_bot_temp_c', 'smt_vwc_pct', 'smt_temp_c'])
+            # Calculate the rate of change of chmln_top_cb, w.r.t the time index
+            # device['chmln_top_cb_rate'] = device['chmln_top_cb'].diff() / device['chmln_top_cb'].index.to_series().diff().dt.total_seconds()
+            # device['chmln_bot_cb_rate'] = device['chmln_bot_cb'].diff() / device['chmln_bot_cb'].index.to_series().diff().dt.total_seconds()
 
-            # ax.set_title('Device: {}'.format(c))
-
+            # Convert the rate from cb/s to cb/h
+            device['chmln_top_cb_rate'] = device['chmln_top_cb_rate'] * 3600
+            device['chmln_bot_cb_rate'] = device['chmln_bot_cb_rate'] * 3600
 
             # Write out the data to mcap file
             for i, row in device.iterrows():
@@ -249,6 +281,12 @@ with open("merged.mcap","wb") as f_mcap:
                     publish_time=t
                 )
 
+                writer.add_message(
+                    column_channel,
+                    log_time=t,
+                    data=row.to_json().encode('utf-8'),
+                    publish_time=t
+                )
 
     writer.finish()
 
