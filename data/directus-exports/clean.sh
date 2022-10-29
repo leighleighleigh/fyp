541cb1224fc4abcc8cf7254060c6f3490fb18033
Leigh Oliver <leighcharlesoliver@gmail.com>
2022-10-29 16:33:04 +1100

    Cleanup for publishing!

--- data/directus-exports/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-./clean_sensor_readings.py --input-file "column_sensors 20221007-84218.json" --output-file "column_sensors_cleaned.json"
-
-tac column_sensors_cleaned.json > cleaned.csv
-
